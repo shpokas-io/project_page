66fc10b1cce75cc4e1b2f0a824e39d6e37c6e789
Skirmantas Spakovskis <skirmantas.spakovskis@ext.bdo.global>
2025-10-07 14:06:12 +0300

    feat: add reusable loader component

---
+++ src/component/ui/Loader.tsx
@@ -0,0 +1,5 @@
+export const Loader = () => (
+  <div className="flex justify-center items-center h-40">
+    <div className="w-8 h-8 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin" />
+  </div>
+);
