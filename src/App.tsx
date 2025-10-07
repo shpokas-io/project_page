cb5dc90afc5b4bbb680b3be79c3b827544025943
Skirmantas Spakovskis <skirmantas.spakovskis@ext.bdo.global>
2025-10-07 10:34:14 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello World 👋 Tailwind + React Query works!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
